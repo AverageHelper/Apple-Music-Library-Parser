a73acb7af70a3dfcb90d2c6f58cd3fab9bf25e86
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-01-10 14:02:38 -0700

    Get filename argument from command execution

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,22 +1,43 @@
 import { version as packageVersion } from "./version";
 
 const mockYargsVersion = jest.fn();
-const mockHelp = jest.fn();
 const mockAlias = jest.fn();
+const mockHelp = jest.fn();
+const mockScriptName = jest.fn();
+const mockUsage = jest.fn();
+const mockDemandCommand = jest.fn();
+const mockFileName = "/foo/bar.baz";
+
 const mockYargs = {
 	version: mockYargsVersion,
 	help: mockHelp,
 	alias: mockAlias,
-	argv: {}
+	scriptName: mockScriptName,
+	usage: mockUsage,
+	demandCommand: mockDemandCommand,
+	argv: {
+		_: [mockFileName] as Array<string | number | null>
+	}
 };
 mockYargsVersion.mockReturnValue(mockYargs);
 mockHelp.mockReturnValue(mockYargs);
 mockAlias.mockReturnValue(mockYargs);
+mockDemandCommand.mockReturnValue(mockYargs);
+mockScriptName.mockReturnValue(mockYargs);
+mockUsage.mockReturnValue(mockYargs);
 jest.mock("yargs", () => mockYargs);
 
+async function start(): Promise<void> {
+	await import("./main");
+}
+
 describe("Main", () => {
-	test("regiters the package version from package.json", async () => {
-		await import("./main");
+	beforeEach(() => {
+		mockYargs.argv["_"] = [mockFileName];
+	});
+
+	test("registers the package version from package.json", async () => {
+		await expect(start()).resolves.not.toThrow();
 		expect(mockYargsVersion).toHaveBeenCalledWith(packageVersion);
 	});
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import "source-map-support/register";
-import yargs from "yargs";
 import { version as packageVersion } from "./version";
+import path from "path";
+import yargs from "yargs";
 
-yargs //
+const { _: positionals } = yargs //
+	.scriptName("apple-music-library-parser")
+	.usage("apple-music-library-parser <path>")
+	.demandCommand(1, "A file path is required")
 	.version(packageVersion)
 	.help()
 	.alias("help", "h").argv;
+
+const filenameArg = positionals[0] ?? "";
+if (typeof filenameArg !== "string") throw new TypeError("A file path is required");
+
+const filename = path.resolve(process.cwd(), filenameArg);
+
+console.log(`Got filename "${filename}"`);
